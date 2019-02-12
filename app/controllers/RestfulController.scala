ee1e79bd7b9f351e50e084c15513bfceb1ee4187
wusi <wusi0109@163.com>
2019-02-12 11:27:05 +0800

    添加 controller 以及 依赖注入 用例

---
+++ app/controllers/RestfulController.scala
@@ -0,0 +1,14 @@
+package controllers
+
+import javax.inject.{Inject, Singleton}
+import model.RestfulRepository
+import play.api.mvc.{AbstractController, AnyContent, ControllerComponents, Request}
+
+@Singleton
+class RestfulController @Inject()(cc: ControllerComponents, repository: RestfulRepository) extends AbstractController(cc) {
+
+
+  def getResult() = Action { implicit request =>
+    Ok(repository.getResults() + "[" + request + "]")
+  }
+}
