02ece5d4a96f88ef825a3447f80e3b061d18e0fa
wusi <wusi0109@163.com>
2019-02-12 17:27:23 +0800

    解析body 并且添加 打印

--- app/controllers/RestfulController.scala
+++ app/controllers/RestfulController.scala
@@ -4,12 +4,16 @@ import java.lang
 
 import javax.inject.{Inject, Singleton}
 import model.{Repository, RestfulRepository}
-import play.api.libs.json.JsValue
+import play.api.libs.json._
 import play.api.mvc.{AbstractController, AnyContent, ControllerComponents, Request}
 
 @Singleton
 class RestfulController @Inject()(cc: ControllerComponents, repository: Repository) extends AbstractController(cc) {
 
+  // 定义类和隐私转换参数
+  case class Person(name: String, age: Int)
+
+  implicit val personReads = Json.reads[Person]
 
   def getResult(id: String) = Action { implicit request =>
     Ok(repository.getResults() + "[" + id + "]")
@@ -23,10 +27,11 @@ class RestfulController @Inject()(cc: ControllerComponents, repository: Reposito
     val body: AnyContent = request.body
     val jsonBody: Option[JsValue] = body.asJson
 
-    jsonBody.map {
-      json => println((json\ "name").as[String])
-    }
+    println((jsonBody.get \ "name").get)
+    println(jsonBody.get("name"))
+    // 反序列化
+    val person: JsResult[Person] = Json.fromJson[Person](jsonBody.get)
+    println(person.get.name)
     Ok("post with body")
   }
-
 }
